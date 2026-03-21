d07701a24f6e7622a12bc185d7d6d6ac98c664e5
Miguel Jose Mata Ramos <miguelmodd@gmail.com>
2026-03-21 13:52:34 -0400

    Add project files.

---
+++ demo.project.site/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace demo.project.site.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
