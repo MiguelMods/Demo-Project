1a32a71bdbb5e932964a96f3de26bbd63c295999
Miguel Jose Mata Ramos <miguelmodd@gmail.com>
2026-03-21 15:43:25 -0400

    agregando dependencias

---
+++ demo.proyect,persistance/Dependencie.cs
@@ -0,0 +1,15 @@
+﻿using demo.proyect_persistance.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace demo.proyect_persistance;
+
+public static class Dependencie
+{
+    public static void AddPersistance(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<DemoProjectApplicationContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("demoProjectDatabaseConnection")));
+    }
+}
