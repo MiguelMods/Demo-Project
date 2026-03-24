362581f26113f71a9b6ad121b2bd41a229e5800f
Miguel Jose Mata Ramos <miguelmodd@gmail.com>
2026-03-23 21:59:34 -0400

    agregando nuevos modelo para update unicamente

---
+++ demo.proyect.application/Update/ProjectUpdate.cs
@@ -0,0 +1,23 @@
+﻿using demo.proyect.application.Create;
+
+namespace demo.proyect.application.Update;
+
+public class ProjectUpdate : BaseCreate
+{
+    public long ProjectId { get; set; }
+    public string CodeOne { get; set; }
+    public string CodeTwo { get; set; }
+    public string Name { get; set; }
+    public string? Objetive { get; set; }
+    public string? Scope { get; set; }
+    public string? Description { get; set; }
+    public long AreaId { get; set; }
+    public long SubAreaId { get; set; }
+    public bool PoaRoadMap { get; set; }
+    public DateTime? WishDate { get; set; }
+    public bool IsCritical { get; set; }
+    public bool UseNormalFlow { get; set; }
+    public long PriorityTypeId { get; set; }
+    public long ProjectTypeId { get; set; }
+    public long ProjectDevelopmentTypeId { get; set; }
+}
