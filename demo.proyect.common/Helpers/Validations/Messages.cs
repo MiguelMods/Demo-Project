862165b3d6203f58784963565ececbe0da97634e
Miguel Jose Mata Ramos <miguelmodd@gmail.com>
2026-03-24 21:29:38 -0400

    agregando mensajes de ayuda

--- demo.proyect.common/Helpers/Validations/Messages.cs
+++ demo.proyect.common/Helpers/Validations/Messages.cs
@@ -3,4 +3,6 @@
 public class Messages
 {
     public const string IsRequired = "Este Campo es Requerido*";
+    public static string MinLength (string min = "1") => $"Este Campo Requiere un Minimo de {min} como Caracter*";
+    public static string MaxLength (string max = "1") => $"Este Campo Requiere un Maximo de {max} como Caracter*";
 }
