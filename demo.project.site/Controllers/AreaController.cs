557bfae0e69fae3e325fb55f4d2f17cf01db977e
Miguel Jose Mata Ramos <miguelmodd@gmail.com>
2026-03-24 21:28:15 -0400

    agregando ventana de area

---
+++ demo.project.site/Controllers/AreaController.cs
@@ -0,0 +1,58 @@
+﻿using demo.project.site.Models.ViewModels;
+using demo.proyect.application.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demo.project.site.Controllers
+{
+    public class AreaController(IUnitOfWork unitOfWork) : Controller
+    {
+        private readonly IUnitOfWork unitOfWork = unitOfWork;
+
+        public async Task<IActionResult> Index()
+        {
+            var areas = await unitOfWork.AreaEntityRepository.GetAllAsync();
+            return View(areas);
+        }
+
+        public async Task<IActionResult> Register()
+        {
+            return View(new AreaViewModel());
+        }
+
+        public async Task<IActionResult> Detail(string rowguid)
+        {
+            var area = await unitOfWork.AreaEntityRepository.GetByRowGuidAsync(rowguid);
+
+            if (area == null)
+                return await Index();
+
+            return View((AreaViewModel)area);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Save(AreaViewModel areaViewModel) 
+        {
+            if(!ModelState.IsValid)
+                return View("register", areaViewModel);
+
+            if(areaViewModel.AreaId > 0) 
+            {
+                areaViewModel.UpdatedBy = "me";
+                var result = await unitOfWork.AreaEntityRepository.UpdateAsync(AreaViewModel.MapUpdate(areaViewModel));
+
+                if (result != null || result?.AreaId > 0)
+                    return View("detail", (AreaViewModel)result);
+            }
+            else 
+            {
+                areaViewModel.CreatedBy = "me";
+                var result = await unitOfWork.AreaEntityRepository.AddAsync(AreaViewModel.MapCreate(areaViewModel));
+
+                if(result != null || result?.AreaId > 0)
+                    return View("detail", (AreaViewModel)result);
+            }
+
+            return await Index();
+        }
+    }
+}
---
+++ demo.project.site/Models/ViewModels/AreaViewModel.cs
@@ -0,0 +1,56 @@
+﻿using demo.proyect.application.Create;
+using demo.proyect.application.DTO_s;
+using demo.proyect.application.Update;
+using demo.proyect.common.Helpers.Validations;
+using System.ComponentModel.DataAnnotations;
+
+namespace demo.project.site.Models.ViewModels;
+
+public class AreaViewModel : BaseViewModel
+{
+    [Display(Name = "Numero De Area")]
+    public long AreaId { get; set; }
+    
+    [Display(Name = "Codigo"), Required(ErrorMessage = Messages.IsRequired)]
+    [MinLength(1, ErrorMessage = "Este Campo Requiere un Minimo de 1 un Caracter*")]
+    [MaxLength(6, ErrorMessage = "Este Campo Requiere un Minimo de 6 un Caracteres*")]
+    public string Code { get; set; }
+    
+    [Display(Name = "Nombre"), Required(ErrorMessage = Messages.IsRequired)]
+    public string Name { get; set; }
+
+    [Display(Name = "Descripcion")]
+    public string? Description { get; set; }
+
+    public static explicit operator AreaViewModel(AreaResponse areaResponse) => new() {
+        AreaId = areaResponse.AreaId,
+        Code = areaResponse.Code,
+        Name = areaResponse.Name,
+        Description = areaResponse.Description,
+        IsActive = areaResponse.IsActive,
+        CreatedAt = areaResponse.CreatedAt,
+        CreatedBy = areaResponse.CreatedBy,
+        UpdatedBy = areaResponse.UpdatedBy,
+        UpdatedAt = areaResponse.UpdatedAt,
+        RowGuid = areaResponse.RowGuid
+    };
+
+    public static AreaCreate MapCreate(AreaViewModel model) => new() { 
+        Code = model.Code,
+        Name = model.Name,
+        Description = model.Description,
+        IsActive = model.IsActive,
+        CreateBy = model.CreatedBy
+    };
+
+    public static AreaUpdate MapUpdate(AreaViewModel model) => new()
+    {
+        AreaId = model.AreaId,
+        Code = model.Code,
+        Name = model.Name,
+        Description = model.Description,
+        IsActive = model.IsActive,
+        UpdateBy = model.UpdatedBy,
+        RowGuid = model.RowGuid
+    };
+}
