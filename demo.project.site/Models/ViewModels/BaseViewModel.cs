2b78b764b97576e795c7e45c131413a89efa861b
Miguel Jose Mata Ramos <miguelmodd@gmail.com>
2026-03-24 21:29:26 -0400

    actualizando clases base/generales de las demas clases

---
+++ demo.project.site/Models/ViewModels/BaseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace demo.project.site.Models.ViewModels;
+
+public abstract class BaseViewModel
+{
+    [Display(Name = "Activo")]
+    public bool IsActive { get; set; } = true;
+    
+    [Display(Name = "Creado Por")]
+    public string? CreatedBy { get; set; }
+
+    [Display(Name = "Fecha Creacion")]
+    public DateTime? CreatedAt { get; set; }
+
+    [Display(Name = "Modificado Ultima Vez Por")]
+    public string? UpdatedBy { get; set; }
+
+    [Display(Name = "Fecha Ultima Modicacion")]
+    public DateTime? UpdatedAt { get; set; }
+    
+    public string? RowGuid { get; set; }
+}
--- demo.proyect.application/Create/BaseCreate.cs
+++ demo.proyect.application/Create/BaseCreate.cs
@@ -2,7 +2,8 @@
 
 public abstract class BaseCreate
 {
+    public bool IsActive { get; set; }
     public string CreateBy {  get; set; }
-    public string UpdateBy { get; set; }
+    public string? UpdateBy { get; set; }
     public string RowGuid { get; set; }
 }
