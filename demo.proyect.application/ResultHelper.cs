8c0617c07d091acb367371cd2bc57c620d5685c8
Miguel Jose Mata Ramos <miguelmodd@gmail.com>
2026-03-26 23:02:26 -0400

    agregando clase de resultados

---
+++ demo.proyect.application/ResultHelper.cs
@@ -0,0 +1,11 @@
+﻿namespace demo.proyect.application;
+
+public static class ResultHelper 
+{ 
+    public static Result<T> Success<T>(this T data) => Result<T>.Success(data);
+    
+    public static Result<bool> Success() => Result<bool>.Success();
+    
+    public static Result<T> Failure<T>(this T data, string message = "Resultado no sastisfactorio")
+    => Result<T>.Failure(message, data);
+}
