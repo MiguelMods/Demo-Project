c3a00fff30cef36610a8eafcacf8d5e7b724892c
Miguel Jose Mata Ramos <miguelmodd@gmail.com>
2026-04-03 12:26:54 -0400

    agregand nuevo controlador empleado

---
+++ demo.project.site/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using demo.proyect.application.Services.Contract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace demo.project.site.Controllers;
+
+public class EmployeeController(IEmployeeService employeeService) : Controller
+{
+    private readonly IEmployeeService employeeService = employeeService;
+
+    public async Task<IActionResult> Index()
+    {
+        var result = await employeeService.GetAllAsync();
+        return View(result.Data);
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public Task<IActionResult> Details(int id)
+    {
+        throw new NotImplementedException();
+    }
+}
